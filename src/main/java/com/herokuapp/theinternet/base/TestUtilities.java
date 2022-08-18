742cdc831ab9ccca52e372671a7ab30eafa35d39
pablopena <pablopena@macbook-pro.gdl.mex.ibm.com>
2022-08-18 15:51:37 -0500

    update to dataprovider

--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -1,5 +1,7 @@
 package com.herokuapp.theinternet.base;
 
+import org.testng.annotations.DataProvider;
+
 public class TestUtilities extends BaseTest{
 
 	//STATIC SLEEP
@@ -11,4 +13,12 @@ public class TestUtilities extends BaseTest{
 		}
 	}
 	
+	@DataProvider(name="files")
+	protected static Object[][] files() {
+		return new Object[][] {
+			{1,"index.html"},
+			{2,"logo.png"},
+			{3,"text.txt"}
+		};
+	}
 }
